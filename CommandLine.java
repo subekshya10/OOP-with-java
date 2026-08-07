dde626543de8aa85ff1ff16474f37a7e8171fd65
subekshya10 <subekshyakc8@gmail.com>
2026-08-07 20:47:43 +0545

    Added DAY2  java programs

---
+++ CommandLine.java
@@ -0,0 +1,6 @@
+public class CommandLine {
+    public static void main(String args[]) 
+    {
+        System.out.println("the first argument is: " + args[0]);
+    }
+    }
---
+++ NewCL.java
@@ -0,0 +1,7 @@
+public class NewCL {
+    public static void main(String args[])
+    {
+        System.out.println("the second argument is:"+args[0]);
+    }
+    }
+    
