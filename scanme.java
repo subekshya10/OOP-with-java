afbca9a844983af05a5b73b0029c83c84d07fa8c
subekshya10 <subekshyakc8@gmail.com>
2026-08-22 21:32:06 +0545

    use of scanner

---
+++ scanme.java
@@ -0,0 +1,11 @@
+ import java.util.Scanner;
+class Scanme
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("please enter your name");
+String name=sc.nextLine();
+System.out.println("my name is:" +name);
+}
+}
