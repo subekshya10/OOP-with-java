3a0684ef9ff985a5b434cc1258444c5fc208edce
subekshya10 <subekshyakc8@gmail.com>
2026-09-06 23:36:14 +0545

    use of variable

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    static int m=100;
+    void method()
+    {
+        int n =90;
+    }
+    public static void main (String args[])
+    {
+        int data =50;
+    }
+    
+}
